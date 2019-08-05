3583ff584ca08d81727ccfb1486b6cd73919af18
krashnerd <krishkringle1@gmail.com>
2019-08-05 16:08:21 -0400

    Created valid click handling for board tiles

--- game_gui.py
+++ game_gui.py
@@ -8,23 +8,73 @@ board_size = 15 * tile_size
 
 mini_square = [(0,0),(0,1),(1,1),(1,0)]
 
+leftclick = '<Button-1>'
 
 class Display():
     def __init__(self, win, game = None):
         self.game = game or Scrabble()
         self.win = win
         self.win.geometry('1000x900+2000+50')
-        self.board = tk.Frame(self.win, width = board_size, height = board_size)
+        self.board = DisplayBoard(master = self.win, game = self.game)
         self.board.grid(padx=5, pady=5, rowspan = 15, columnspan = 15)
-        self.board_squares = [[None] * 15 for _ in range(15)]
+
+        self.rack = DisplayRack(master = self.win, game = self.game)
+        self.rack.grid(padx=5, pady = 5, row = 16, columnspan = 7)
 
 
-    def full_text(bonus_code):
+        closebutton = tk.Button(self.win, text="close", width=10, command=self.win.destroy)
+        closebutton.grid(column = 0, sticky = SW)
+        self.board.create_board()
 
-        b_type, b_number = list(bonus_code)
+
+    def place_tile(tile, location):
+        """ Move a tile to a given location"""
+        pass
+
+class DisplayRack(tk.Frame):
+    def __init__(self, master, game):
+        self.master = master
+        self.game = game
+
+        super().__init__(self.master, width = tile_size * 7, height = tile_size)
+
+    def handle_tile_click():
+        pass
+
+    def refill_rack(self):
+        for ind, tile in enumerate(self.game.current_player.rack):
+            pass
+
+
+
+class DisplayBoard(tk.Frame):
+    def __init__(self, master, game):
+        self.game = game or Scrabble()
+        self.game_board = self.game.board
+        self.master = master
+
+        super().__init__(self.master, width = board_size, height = board_size)
+        
+        self.squares = [[None] * 15 for _ in range(15)]
+        self.current_move = []
+        self.focus_tile = []
+
+    
+
+    def swap_tiles_at(loc_1, loc_2):
+        r1,c1 = loc_1
+        r2,c2 = loc_2
+        self.squares[r1][c1].grid(loc_2)
+        self.squares[r2][c2].grid(loc_1)
+
+    def handle_click(self, r, c):
+        """ Handle click on the square at (r, c)"""
+        if self.game_board[r,c].occupied:
+            return
+
+        print("Clicked on {}".format((r,c)))
 
     def create_board(self):
-        board = self.game.board
         bonus_color_lookup = {
                     'L2':'Cyan',
                     'W2':'Pink',
@@ -38,37 +88,47 @@ class Display():
                     'W3':'Triple\nword\nscore',
                     }
 
+        def get_click_function(x, y):
+            def func(event):
+                self.handle_click(x, y)
+            return func
+
         for c in range(15):
             for r in range(15):
-                tile = board[r,c]
+                tile = self.game_board[r,c]
                 coords = [((r + a) * tile_size, (c + b) * tile_size) for a,b in mini_square]
                 x, y = ((r + .5) * tile_size, (c + .5) * tile_size)
-
                 bonus = tile.bonusType
                 bg_color = bonus_color_lookup.get(bonus, 'beige')
                 text_color = "white" if bg_color in ("Blue", "Red") else "black"
-                square = tk.Canvas(self.board, width = tile_size, height = tile_size, bg = bg_color, 
+
+                square = tk.Canvas(self, width = tile_size, height = tile_size, bg = bg_color, 
                     borderwidth = 1, relief = tk.RIDGE, 
                     highlightcolor = 'black', highlightbackground = 'black')
+
+                func = get_click_function(r, c)
+
+                square.bind(leftclick, func)
+
                 bonus_text = bonus_text_lookup.get(bonus, None)
                 if bonus_text:
-                    text = square.create_text(tile_center, text = bonus_text, font = ("Arial", 10), fill = text_color)
+                    text = square.create_text(tile_center, text = bonus_text, font = ("Arial", 10), fill = text_color, justify = tk.CENTER)
 
 
                 square.grid(row = r, column = c)
-                self.board_squares[r][c] = square
+                self.squares[r][c] = square
 
                 text_color = "white" if bonus in ("L3", "W3") else "black"
 
-        closebutton = tk.Button(self.win, text="close", width=10, command=self.test_pack)
-        closebutton.grid(column = 0, sticky = SW)
+
+
+        
 
 
 
 def main():
     win = tk.Tk()
     display = Display(win)
-    display.create_board()
     win.mainloop()
 
 
